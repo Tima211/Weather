e0e3c9a0399856e052d04da7bf78366ae60e1789
Tima211 <nastyatimofeeva91108@gmail.com>
2025-09-28 12:01:17 +0800

    script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+const apiKey = '4f77687dc11419a6c932743e4ce3be51';
+const cityInput = document.getElementById('city-input');
+const searchBtn = document.getElementById('search-btn');
+const weatherInfo = document.getElementById('weather-info');
+
+searchBtn.addEventListener('click', getWeather);
+
+async function getWeather() {
+    const city = cityInput.value.trim();
+    if (!city) return;
+
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=ru`;
+
+    try {
+      const response = await fetch(apiUrl);
+      const data = awat responce.json();
+
+      if (resonce.ok) {
+        dasplayWeather(data;
+      } else {
+          weatherInfo.innerHTML = `<p>Город не найден. Проверьте название.</p>`;
+      }
+   } cath (error) {
+       console.error('Ошибка:', error);
+       weatherInfo.innerHTML = `<p>Что-то пошло не так...</p>`;
+   }
+}
+
+function displayWeather(data) {
+  const { name } = data;
+  const { description, icon } = data.weather[0];
